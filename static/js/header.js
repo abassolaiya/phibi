9d09af6e175a917b58b19ed08d456a139a77fd84
abassolaiya <ayotundeolaiya@gmail.com>
2021-07-01 14:09:58 +0100

    Add files via upload

---
+++ static/js/header.js
@@ -0,0 +1,10 @@
+function openNav() {
+    document.getElementById("top_header_totalFixed").style.display = 'block';
+    document.getElementById("top_header_totalFixed_main").style.transform = "translateY(0rem)";
+  }
+
+
+  function closeNav() {
+    document.getElementById("top_header_totalFixed").style.display = 'none';
+    document.getElementById("top_header_totalFixed_main").style.transform = "translateY(100rem)";
+  }
---
+++ static/js/index.js
@@ -0,0 +1,10 @@
+function opencenter() {
+    document.getElementById("backdrop").style.display = 'flex';
+    document.getElementById("backdrop").style.transform = "translateX(0rem)";
+  }
+  
+  function closecenter() {
+    document.getElementById("backdrop").style.display = 'none';
+    document.getElementById("backdrop").style.transform = "translateX(-18rem)";
+
+  }
